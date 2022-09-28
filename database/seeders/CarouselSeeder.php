1581d9c55afe06b92e599c1cffad01951464ff2e
Kieran Noble <kieran@aylo.net>
2022-09-28 09:12:14 +0100

    Fix PSR-4 autoload, and NPM & Composer versions, and update seeded media

--- database/seeders/CarouselSeeder.php
+++ database/seeders/CarouselSeeder.php
@@ -17,27 +17,12 @@ class CarouselSeeder extends Seeder
         DB::table('carousels')->insert([
             'link' => "/music/7",
             'description' => "Ariana is back with her new album Positions",
-            'image' => "static/positionsbanner.jpg",
+            'image' => "static/positionsbannernew.jpg",
         ]);
         DB::table('carousels')->insert([
             'link' => "/music",
             'description' => "Lot of new music released for you to enjoy",
-            'image' => "static/newmusicbanner.png",
-        ]);
-        DB::table('carousels')->insert([
-            'link' => "/music/1",
-            'description' => "Hit singer-songwriter Lorde with her new album Melodrama",
-            'image' => "static/lordebanner.jpg",
-        ]);
-        DB::table('carousels')->insert([
-            'link' => "/merch/1",
-            'description' => "With the stylish Hoodie, Ariana Grande has a collection of new merch",
-            'image' => "static/positionsbanner2.png",
-        ]);
-        DB::table('carousels')->insert([
-            'link' => "/events/3",
-            'description' => "See them live! McFly are touring again!",
-            'image' => "static/mcflybanner.png",
+            'image' => "static/newmusicbannernew.jpg",
         ]);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,11 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
     darkMode: 'media',
-
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
     theme: {
         fontFamily: {
             'poppins': ['Poppins', 'sans-serif'],
